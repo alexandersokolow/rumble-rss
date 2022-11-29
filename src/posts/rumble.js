3d35f9c131e817905b5ed11252963f470d83b911
alexandersokolow <alexandersokolow96@gmail.com>
2022-11-29 18:01:39 +0100

    add fallback for hidden views to rumble feed

--- src/posts/rumble.js
+++ src/posts/rumble.js
@@ -11,7 +11,7 @@ const getRumblePosts = async (channelName) => {
     const url_rel = $(".video-item--a", item).attr("href");
     const url_abs = `https://rumble.com${url_rel}`;
     const date = $(".video-item--time", item).attr("datetime");
-    const views = $(".video-item--views", item).attr("data-value");
+    const views = $(".video-item--views", item).attr("data-value") || "hidden";
     const duration = $(".video-item--duration", item).attr("data-value");
     const description = `Views: ${views}<br/>Duration: ${duration}`;
     return { title, link: url_abs, date, description };
